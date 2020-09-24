2b101d065526abe46dc20af8bb7650c597f1117f
TJ Miller <tmmiller3@bsu.edu>
2020-09-24 15:04:33 -0400

    Updated build.gradle, added .json samples, fixed directories

--- src/test/java/firstProjectTest/test.java
+++ src/test/java/firstProjectTest/test.java
@@ -2,9 +2,19 @@ package firstProjectTest;
 
 import org.junit.Test;
 
+import java.awt.*;
+import java.util.ArrayList;
+import java.util.Date;
+
 public class test {
     @Test
     public void testAPI(){
+        ArrayList<String>
+        ArrayList<Date>
+    }
+    @Test
+    public void testRequest(){
+        String articleUserInput = "Soup";
 
     }
 }
