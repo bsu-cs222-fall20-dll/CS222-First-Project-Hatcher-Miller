b9dd09f56195dcc3df229fa4b89b62aa645f9c76
Ethan Hatcher <erhatcher@bsu.edu>
2020-09-24 15:29:52 -0400

    Properly added imports, acquired gson-2.6.2.jar library, cleaned up test file

--- src/test/java/firstProjectTest/test.java
+++ src/test/java/firstProjectTest/test.java
@@ -1,20 +1,23 @@
 package firstProjectTest;
 
+import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
+import org.junit.Assert;
 import org.junit.Test;
 
-import java.awt.*;
+import java.io.InputStream;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonParser;
+import java.io.InputStreamReader;
+import java.io.Reader;
 import java.util.ArrayList;
 import java.util.Date;
+import java.util.Map;
 
 public class test {
     @Test
-    public void testAPI(){
-        ArrayList<String>
-        ArrayList<Date>
-    }
-    @Test
-    public void testRequest(){
-        String articleUserInput = "Soup";
+    public void testJSON(){
+        JsonParser parser = new JsonParser();
 
     }
 }
