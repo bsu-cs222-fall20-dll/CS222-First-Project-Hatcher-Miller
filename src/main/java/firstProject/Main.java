9eb503db2b2a3d99520d6bd7fbda898f6bdadf7c
TJ Miller <tmmiller3@bsu.edu>
2020-09-28 15:11:27 -0400

    Created the menu and main classes. Ensured proper functioning through running before committing.

---
+++ src/main/java/firstProject/Main.java
@@ -0,0 +1,14 @@
+package firstProject;
+
+import java.net.URL;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        URLConnection urlConnection = new URLConnection();
+        Menu menu = new Menu();
+        URLSearchResults urlSearchResults = new URLSearchResults();
+        String userInput = menu.getUserInput();
+        URL url = urlConnection.insertInputToURLConverter(userInput);
+        urlSearchResults.RevisionList(urlConnection.acquireConnectionToWikipedia(url));
+    }
+}
---
+++ src/main/java/firstProject/Menu.java
@@ -0,0 +1,12 @@
+package firstProject;
+
+import java.util.Scanner;
+
+public class Menu {
+    public String getUserInput() {
+        Scanner console = new Scanner(System.in);
+        System.out.println("Please enter your desired search term(s):");
+        String userSearch = console.nextLine();
+        return userSearch;
+    }
+}
