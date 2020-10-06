fd35a78a6e6fbd5633fb6f79300808ed1a94abf5
TJ Miller <tmmiller3@bsu.edu>
2020-10-05 20:18:58 -0400

    Updated RevisionParser to function properly and add functionality requirements from iteration 1 rubric

--- src/main/java/edu/bsu/cs222/RevisionParser.java
+++ src/main/java/edu/bsu/cs222/RevisionParser.java
@@ -5,30 +5,34 @@ import com.google.gson.JsonArray;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
+import javafx.scene.control.Label;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+
 
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.Reader;
-import java.lang.reflect.Array;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.Map;
+import java.util.TimeZone;
 
 public class RevisionParser {
     ArrayList<Revisions> listOfRevisions = new ArrayList<>();
-    public ArrayList<Revisions> FullListOfRevisions(InputStream inputStream) {
-        //InputStream inputStream2 = inputStream;
-        JsonElement rootElement = getRootElement(inputStream);
-        tryRedirect(rootElement);
-
-        try {
-            JsonObject pages = createJsonParser(rootElement);
-            //checkForRedirects(inputStream);
-            JsonArray revisionArray = createJsonArray(pages);
+    public ArrayList<Revisions> FullListOfRevisions(InputStream inputStream, VBox parent) {
+        try{
+            JsonElement rootElement = getRootElement(inputStream);
+            tryRedirect(rootElement, parent);
+            JsonObject pages = createJsonParserForWikipedia(rootElement);
+            JsonArray revisionArray = createJsonArrayForRevisions(pages);
             listOfRevisions = createListOfRevisions(revisionArray);
             return listOfRevisions;
         }catch(Exception e){
-            e.printStackTrace();
-            System.out.println("Page was not found");
+            HBox pageNotFound = new HBox(new Label("Page was not found"));
+            parent.getChildren().add(pageNotFound);
             return null;
         }
     }
@@ -37,17 +41,15 @@ public class RevisionParser {
     private JsonElement getRootElement(InputStream inputStream) {
         JsonParser parser = new JsonParser();
         Reader reader = new InputStreamReader(inputStream);
-        JsonElement rootElement = parser.parse(reader);
-        return rootElement;
+        return parser.parse(reader);
     }
 
-    private JsonObject createJsonParser(JsonElement rootElement) {
+    private JsonObject createJsonParserForWikipedia(JsonElement rootElement) {
         JsonObject rootObject = rootElement.getAsJsonObject();
-        JsonObject pages = rootObject.getAsJsonObject("query").getAsJsonObject("pages");
-        return pages;
+        return rootObject.getAsJsonObject("query").getAsJsonObject("pages");
     }
 
-    private JsonArray createJsonArray(JsonObject pages) {
+    private JsonArray createJsonArrayForRevisions(JsonObject pages) {
         JsonArray revisionArray = null;
         for (Map.Entry<String, JsonElement> entry : pages.entrySet()) {
             JsonObject entryObject = entry.getValue().getAsJsonObject();
@@ -56,30 +58,48 @@ public class RevisionParser {
         return revisionArray;
     }
 
-    private ArrayList<Revisions> createListOfRevisions(JsonArray revisionArray) {
+    private ArrayList<Revisions> createListOfRevisions(JsonArray revisionArray)  throws ParseException{
         for (JsonElement entry : revisionArray) {
             String user = entry.getAsJsonObject().get("user").getAsString();
             String timeStamp = entry.getAsJsonObject().get("timestamp").getAsString();
-            Revisions revision = new Revisions(user, timeStamp);
+            String properTimeStamp = accountForLocalUserTimeZone(timeStamp);
+            Revisions revision = new Revisions(user, properTimeStamp);
             listOfRevisions.add(revision);
         }
         return listOfRevisions;
     }
 
-    @SuppressWarnings("deprecation")
-    private void checkForRedirects(JsonElement rootElement) {
+    public String accountForLocalUserTimeZone(String timeStamp) {
+        String convertTimeStamp = timeStamp.replaceAll("T", " ");
+        convertTimeStamp = convertTimeStamp.replaceAll("Z", "");
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        TimeZone currentTimeZone = TimeZone.getDefault();
+        simpleDateFormat.setTimeZone(currentTimeZone);
+        Date date;
+        try {
+            date = simpleDateFormat.parse(convertTimeStamp);
+            SimpleDateFormat simpleDateFormat1;
+            simpleDateFormat1 = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+            return simpleDateFormat1.format(date);
+        }catch(ParseException e) {
+            System.out.println(convertTimeStamp);
+            return timeStamp;
+        }
+    }
+
+    private void checkForRedirects(JsonElement rootElement, VBox parent) {
         JsonObject rootObject = rootElement.getAsJsonObject();
         JsonObject redirects = rootObject.getAsJsonObject("query").getAsJsonArray("redirects").get(0).getAsJsonObject();
         String from = redirects.get("from").getAsString();
         String to = redirects.get("to").getAsString();
-        System.out.println("From: " + from + "\nTo: " + to + "\n");
+        HBox redirect = new HBox(new Label("From: " + from + "\nTo: " + to + "\n"));
+        parent.getChildren().add(redirect);
     }
 
-    private void tryRedirect(JsonElement rootElement) {
+    private void tryRedirect(JsonElement rootElement, VBox parent) {
         try{
-            checkForRedirects(rootElement);
-        }catch(Exception f){
-            System.out.println("here");
+            checkForRedirects(rootElement, parent);
+        }catch(Exception ignored){
         }
     }
 }
