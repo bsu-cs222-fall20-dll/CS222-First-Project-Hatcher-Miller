13b857ca7094aedd7e01909f32f161bdb8a15b51
TJ Miller <tmmiller3@bsu.edu>
2020-10-05 20:26:42 -0400

    Deleted DisplayMostFrequentAuthor.java class since it is no longer compatible with program functionality and is therefore no longer necessary

--- src/main/java/edu/bsu/cs222/DisplayMostFrequentAuthor.java
+++
@@ -1,9 +0,0 @@
-package edu.bsu.cs222;
-
-import java.util.ArrayList;
-
-public class DisplayMostFrequentAuthor extends AuthorListParser{
-    public void DisplayFrequentAuthor(){
-        ArrayList<Author> authorList = parseJSON();
-    }
-}
