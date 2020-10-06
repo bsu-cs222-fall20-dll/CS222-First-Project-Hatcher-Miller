2fd14ecf9b3fa6eb3455e78a5582c80d931e0867
TJ Miller <tmmiller3@bsu.edu>
2020-10-05 20:17:02 -0400

    Updated URL Connection class, added jfx libraries for gui

--- src/main/java/edu/bsu/cs222/URLConnection.java
+++ src/main/java/edu/bsu/cs222/URLConnection.java
@@ -1,23 +1,27 @@
 package edu.bsu.cs222;
 
+import javafx.scene.control.Label;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+
 import java.io.InputStream;
 import java.net.URL;
 
 public class URLConnection {
-    public InputStream acquireConnectionToWikipedia(URL url) {
+    public InputStream acquireConnectionToWikipedia(URL url, VBox parent) {
         try {
             java.net.URLConnection connection = url.openConnection();
             connection.setRequestProperty("User-Agent", "Revision Tracker/0.1 (tmmiller3@bsu.edu)");
             return connection.getInputStream();
         }catch(Exception e) {
-            System.out.println("The search you have chosen does not exist. Please try again.");
+            HBox noWikipediaConnection = new HBox(new Label("Failed to connect to Wikipedia"));
+            parent.getChildren().add(noWikipediaConnection);
         }
         return null;
     }
 
     public URL insertInputToURLConverter(String webSearch) throws Exception {
         String convert = webSearch.replaceAll(" ", "%20");
-        System.out.println(convert);
         URL url = new URL("https://en.wikipedia.org/w/api.php?action=query&format=json&prop=revisions&titles=" + convert + "&rvprop=timestamp|user&rvlimit=20&redirects");
         return url;
     }
