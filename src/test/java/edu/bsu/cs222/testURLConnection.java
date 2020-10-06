928281d9f208c22003cf0e087a6905549c480a8c
TJ Miller <tmmiller3@bsu.edu>
2020-10-05 20:17:54 -0400

    Updated testURLConnection file to associate correctly with URL Connection

--- src/test/java/edu/bsu/cs222/testURLConnection.java
+++ src/test/java/edu/bsu/cs222/testURLConnection.java
@@ -11,7 +11,6 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.URL;
 import java.net.URLConnection;
-import java.util.ArrayList;
 import java.util.Map;
 
 public class testURLConnection {
